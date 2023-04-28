494477942504189ab4530ff59cd241d3a0744778
MoYVR <78653180+MoYVR@users.noreply.github.com>
2023-04-27 18:33:13 -0700

    modified to be per volume

--- cleanup-snapshots.py
+++ cleanup-snapshots.py
@@ -4,20 +4,40 @@ from operator import itemgetter
 
 ec2_client = boto3.client('ec2', region_name='us-west-1')
 
-snapshots = ec2_client.describe_snapshots(
-    OwnerIds=['self']
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'tag:Name',
+            'Values': ['prod']
+        }
+    ]
 )
 
-sorted_by_date = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
+for volume in volumes['Volumes']:
+    snapshots = ec2_client.describe_snapshots(
+        OwnerIds=['self'],
+        Filters=[
+            {
+                'Name': 'volume-id',
+                'Values': [volume['VolumeId']]
+            }
+        ]
+    )
 
-def deleteing_snapshots():
-    for snap in sorted_by_date[2:]:
-        response = ec2_client.delete_snapshot(
-            SnapshotId=snap['SnapshotId']
-        )
-        print(response)
 
-schedule.every(20).seconds.do(deleteing_snapshots)
+    sorted_by_date = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
 
-while True:
-    schedule.run_pending()
+
+    def deleteing_snapshots():
+        for snap in sorted_by_date[2:]:
+            response = ec2_client.delete_snapshot(
+                SnapshotId=snap['SnapshotId']
+            )
+            print(response)
+
+
+    schedule.every(20).seconds.do(deleteing_snapshots)
+
+    while True:
+        schedule.run_pending()
+
